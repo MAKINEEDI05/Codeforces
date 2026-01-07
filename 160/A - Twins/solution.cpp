86a0d7b2576d8138c4fa91d1373f21cc8f845468
MAKINEEDI LAKSHMI NARAYANA <142885683+MAKINEEDI05@users.noreply.github.com>
2026-01-07 21:51:03 +0530

    Add Twins [A] from Codeforces

---
+++ 160/A - Twins/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;int main(){	int n;	cin >> n;	int a=0,b=0;	int i=0;	vector<int> A(n);	for(i=0;i<n;i++){		cin >> A[i];		a+=A[i];	}	sort(A.begin(),A.end());	for(i=n-1;i>=0;i--){		if(b>a){			break;		}		else{			a-=A[i];			b+=A[i];		}	}	cout << n-i-1; }

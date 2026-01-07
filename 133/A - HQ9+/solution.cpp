e0936e950a6f5bf2f43a3331296752e6d7e96676
MAKINEEDI LAKSHMI NARAYANA <142885683+MAKINEEDI05@users.noreply.github.com>
2026-01-07 22:28:02 +0530

    Add HQ9+ [A] from Codeforces

---
+++ 133/A - HQ9+/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;int main(){	int a=0;	string A;	cin >> A;	for(auto i:A){		if(i=='H' || i=='Q' || i=='9'){			a=1;			break;		}	}	if(a==1){		cout << "YES";	}	else{		cout << "NO";	}}
